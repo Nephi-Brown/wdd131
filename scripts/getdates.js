02bb855c26461545a3632adcb4e0548b284665d8
Nephi-Brown <nephi1brown@gmail.com>
2025-03-22 03:33:21 +0000

    Add files via upload

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,12 +2,5 @@ const year = document.querySelector("#currentyear");
 const today = new Date();
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
-
-//let lastModifiedParagraph = new Date(document.lastModified);
-//const lastModifiedParagraph = document.getElementById('lastModified');
-//document.querySelector("#lastModified") = lastModifiedParagraph;
-//const lastModifiedDate = new Date(document.lastModified);
-//::contentReference[oaicite:28]{index=28}
-
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = lastModified;
