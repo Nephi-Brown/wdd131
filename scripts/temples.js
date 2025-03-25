2e6a842743defcb9a30937162eaac2975759e035
Nephi-Brown <nephi1brown@gmail.com>
2025-03-25 22:07:58 +0000

    Add files via upload

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
+
+const mainnav = document.querySelector('.navigation');
+const hamButton = document.querySelector('#menu');
+
+hamButton.addEventListener('click', () => {mainnav.classList.toggle('show'); hamButton.classList.toggle('show');});   
