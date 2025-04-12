b1ab1b3ebab4185f358ca1ee0cc24ef2f40fd43f
Nephi-Brown <nephi1brown@gmail.com>
2025-04-13 00:13:50 +0100

    Add files via upload

--- scripts/form.js
+++ scripts/form.js
@@ -23,13 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("review-count").textContent = reviewCount;
 });
 
-// Dynamically set the current year in the footer......
 const currentYearElement = document.getElementById("currentyear");
 if (currentYearElement) {
     currentYearElement.textContent = new Date().getFullYear();
 }
 
-// Display the last modified date in the footer.....
 const lastModifiedElement = document.getElementById("lastModified");
 if (lastModifiedElement) {
     lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
